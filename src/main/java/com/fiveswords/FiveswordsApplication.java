73bd5e6d6911374f068dfa7abf44532a1f1dc6ec
Joao Veras <joaohg.veras@gmail.com>
2019-05-04 20:25:17 -0300

    first commit

---
+++ src/main/java/com/fiveswords/FiveswordsApplication.java
@@ -0,0 +1,13 @@
+package com.fiveswords;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FiveswordsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FiveswordsApplication.class, args);
+    }
+
+}
